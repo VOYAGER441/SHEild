84cfafc0d28669f3e99711eef527414b9f40cea7
[Mainak Banduri] <[mainak407@gmail.com]>
2025-05-30 00:23:31 +0530

    Remove unused tab components and update NotFound screen for improved user experience

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function index() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Welcome to Expo Router!</Text>
+            <Text>index</Text>
+        </View>
+  
+
+  )
+}
