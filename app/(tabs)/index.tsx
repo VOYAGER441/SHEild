2052d1beafda0fa3f6e12b216479ff8b19d3ca53
VOYAGER441 <maianka407@gmail.com>
2025-08-13 15:54:58 +0530

    fix: update Metro and Tailwind configurations for improved styling
    - Adjusted Metro configuration to reference the correct global CSS file path.
    - Enhanced Tailwind configuration to include views directory for content scanning.
    - Modified index.tsx to update background and text colors for better visibility.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from "react-native";
 
 export default function Page() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-white text-lg">Hello NativeWind</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500 text-lg">Hello NativeWind</Text>
     </View>
   );
 }
